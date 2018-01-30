abd1e889deb4206810b3c4532a93855ea9acaf11
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-01-30 09:55:18 +0100

    spectra delegate

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -10,8 +10,5 @@ ddosa.CacheStack.append(cache)
 #ddosa.BinEventsImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 #ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
-ddosa.ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-ddosa.CatExtract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
