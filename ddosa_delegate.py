913285846caea1e291a498be556f99fcb27df41d
Volodymyr <vladimir.savchenko@gmail.com>
2018-03-01 11:23:32 +0100

    delegate image and spectrum

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -2,13 +2,11 @@ import os
 import ddosa
 import dataanalysis.caches.queue as queue
 
-cache=queue.QueueCache(os.environ['HOME']+"/ddosa-queue")
+cache=queue.QueueCache(os.environ['DDA_QUEUE'])
 
 ddosa.CacheStack[-1].parent=cache
 ddosa.CacheStack.append(cache)
 
-#ddosa.BinEventsImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-#ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-
+ddosa.ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
