3165a7467191b1e69812d4e4d9ec533e6ede3978
Volodymyr <vladimir.savchenko@gmail.com>
2018-03-02 22:06:39 +0100

    also GB and gti

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -8,5 +8,8 @@ ddosa.CacheStack[-1].parent=cache
 ddosa.CacheStack.append(cache)
 
 ddosa.ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+
 ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
