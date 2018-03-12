e154658b8b9c3e4fe1ff27fd63a90a05dc7b7892
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2018-03-12 15:15:39 +0100

    delegation introcution

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -3,13 +3,18 @@ import ddosa
 import dataanalysis.caches.queue as queue
 
 cache=queue.QueueCache(os.environ['DDA_QUEUE'])
+cache.delegate_by_default=True
 
 ddosa.CacheStack[-1].parent=cache
 ddosa.CacheStack.append(cache)
 
-ddosa.ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+class ii_skyimage(ddosa.ii_skyimage):
+    read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+
 ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
 ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+
+ddosa.mosaic_ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
