69c0676f3e41d2131585bdbb81c06aa398ad2021
Volodymyr <vladimir.savchenko@gmail.com>
2018-03-12 17:49:59 +0100

    only image

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -11,8 +11,8 @@ ddosa.CacheStack.append(cache)
 class ii_skyimage(ddosa.ii_skyimage):
     read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
-ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+#ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+#ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
 ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
