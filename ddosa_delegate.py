44e45609530e2dbfc5ced7cd1eab6f8894bfe248
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-10-01 15:49:53 +0200

    less delegations

--- ddosa_delegate.py
+++ ddosa_delegate.py
@@ -14,25 +14,27 @@ print("CacheStack:", ddosa.CacheStack)
 class ii_skyimage(ddosa.ii_skyimage):
     read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
-ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-
-ddosa.BinEventsSpectra.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 ddosa.ii_spectra_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-
-ddosa.mosaic_ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-
 ddosa.ii_lc_extract.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
-try:
-    da.byname('ii_light').__class__.ii_light.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-except Exception:
-    pass
+if False:
+    ddosa.ghost_bustersImage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+    ddosa.ibis_gti.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
 
-try:
-    da.byname('jemx_image').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-    da.byname('jemx_spe').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-    da.byname('jemx_lcr').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
-except Exception:
-    pass
+    ddosa.BinEventsSpectra.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+
+    #ddosa.mosaic_ii_skyimage.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+
+
+    try:
+        da.byname('ii_light').__class__.ii_light.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+    except Exception:
+        pass
+
+    try:
+        da.byname('jemx_image').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+        da.byname('jemx_spe').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+        da.byname('jemx_lcr').__class__.read_caches=[queue.QueueCache]+list(ddosa.CatExtract.read_caches)
+    except Exception:
+        pass
 
